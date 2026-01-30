13b1b8fa355612135ba3584a70577ff2246c60d5
Silveira <gabrielsantossilveira@hotmail.com>
2026-01-30 15:31:12 -0300

    criação da entidade Perfil

---
+++ src/main/java/com/habitos/gestor_habitos/model/Perfil.java
@@ -0,0 +1,26 @@
+package com.habitos.gestor_habitos.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "perfis")
+public class Perfil {
+
+    public Perfil() {
+        this.nivel = 1;
+        this.pontosExperiencia = 0;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String nomeExibicao;
+    private String bio;
+
+    private Integer nivel;
+    private Integer pontosExperiencia;
+}
