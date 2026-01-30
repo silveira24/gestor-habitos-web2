cc6988f1698f5bfae6afafa9ef582e1bd7471cbc
Silveira <gabrielsantossilveira@hotmail.com>
2026-01-30 13:06:14 -0300

    initial commit

---
+++ src/main/java/com/habitos/gestor_habitos/GestorHabitosApplication.java
@@ -0,0 +1,13 @@
+package com.habitos.gestor_habitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorHabitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorHabitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habitos/gestor_habitos/GestorHabitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitos.gestor_habitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorHabitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
