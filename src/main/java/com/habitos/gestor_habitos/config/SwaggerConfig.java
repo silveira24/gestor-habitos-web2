a13c5edca04bc9471f469be90b95155de8d3a3d9
Silveira <gabrielsantossilveira@hotmail.com>
2026-01-30 15:30:41 -0300

    alteração nas configurações do swagger

---
+++ src/main/java/com/habitos/gestor_habitos/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.habitos.gestor_habitos.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gestor de Hábitos API")
+                        .version("1.0.0")
+                        .description("API para gerenciamento de hábitos e acompanhamento de metas pessoais.")
+                        .contact(new Contact()
+                                .name("Gabriel Silveira e Maria Luiza Câmara")
+                                .url("https://github.com/silveira24/gestor-habitos-web2"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                );
+    }
+}
